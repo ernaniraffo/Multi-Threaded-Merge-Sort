c65116931cb12f75cd81b10bb66671e8882046a6
ernaniraffo <ernani.raffo@icloud.com>
2023-03-26 19:56:25 -0500

    plot to latex

--- plot.sh
+++ plot.sh
@@ -3,7 +3,7 @@
 make clean && make
 rm -f /tmp/results.dat
 
-MAX=100000000
+MAX=1000000
 
 echo "Running sort program..."
 for ((i=1000; $i<=$MAX; i*=10)); do
@@ -15,21 +15,21 @@ done
 
 awk '($1 == "Parallel") { print $4, $6 }' /tmp/results.dat >/tmp/Parallel.dat
 awk '($1 == "Merge") { print $3, $5 } ' /tmp/results.dat >/tmp/Merge.dat
+awk '($1 == "Optimized") { print $5, $7 }' /tmp/results.dat >/tmp/Optimized.dat
 
 echo "Plotting..."
 gnuplot << EOF
-set terminal pdf
-set title "Parallel vs. Sequential Merge Sort"
-set term pdfcairo font "Times-New-Roman,12"
+set terminal epslatex color
+set title "Parallel, Optimized Parallel, Sequential"
 set xlabel "Elements"
 set ylabel "Time (seconds)"
 set logscale
 set xtics nologscale
 set ytics nologscale
 set yrange[:100]
-set output "parallel-vs-sequential.pdf"
+set output "time.tex"
 plot "/tmp/Parallel.dat" using 1:2 title 'Parallel' with lines , \
+"/tmp/Optimized.dat" using 1:2 title 'Optimized Parallel' with lines , \
 "/tmp/Merge.dat" using 1:2 title 'Sequential' with lines
 EOF
 echo "Done."
-echo "Opening plot..."
