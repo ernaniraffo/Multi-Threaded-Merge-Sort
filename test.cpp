7775cbfb1a95a0422ea79a77331307d5af3ba42d
ernaniraffo <ernani.raffo@icloud.com>
2024-01-16 23:00:58 -0800

    Test NumberGenerator and ArrayGenerator

--- test.cpp
+++ test.cpp
@@ -1,5 +1,31 @@
-//
-// Created by Ernani Raffo on 1/16/24.
-//
+#include "number_generator.h"
+#include "array_generator.h"
 
-#include "test.h"
+#include <iostream>
+
+int main(void) {
+    NumberGenerator rng = NumberGenerator(0, 500);
+
+    uint32_t lo = rng.number();
+    uint32_t hi = rng.number();
+    while (hi < lo) {
+        hi = rng.number();
+    }
+
+    for (int i = 0; i < 10; i += 1) {
+        std::cout << rng.number() << "\n";
+    }
+
+    ArrayGenerator array_generator = ArrayGenerator(lo, hi);
+    std::vector<uint32_t> A(50);
+    array_generator.arrayGen(A);
+
+    std::cout << "lo = " << lo << ", hi = " << hi << "\n";
+    std::cout << "[";
+    for (int i = 0; i < (int) A.size(); i += 1) {
+        std::cout << A[i] << (i + 1 == (int) A.size() ? "" : ", ");
+    }
+    std::cout << "]\n";
+
+    return 0;
+}
