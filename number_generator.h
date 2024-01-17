0aec8fe53dd671cbd9d551962341cd20ca0fa4fd
ernaniraffo <ernani.raffo@icloud.com>
2024-01-16 22:59:53 -0800

    Number generator class

---
+++ number_generator.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+#include <random>
+
+
+class NumberGenerator {
+public:
+    NumberGenerator();
+    NumberGenerator(uint32_t lo, uint32_t hi);
+    uint32_t number();
+private:
+    uint32_t lo;
+    uint32_t hi;
+    std::mt19937 gen;
+    std::uniform_int_distribution<uint32_t> distrib;
+};
